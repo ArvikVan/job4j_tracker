564aeaba4d1fc23546edebf3e8bee729c2403cf1
ArvikVan <arvikvan@gmail.com>
2021-03-07 08:30:34 +0300

    7. Метод удаления заявки Tracker.delete [#475877]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -70,7 +70,7 @@ public class Tracker {
         boolean rsl = index != 1;
         if (rsl) {
             items[index] = null;
-            System.arraycopy(items, index + 1, items, index, items.length - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
