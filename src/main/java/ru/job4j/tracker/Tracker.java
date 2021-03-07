b8936059d38b561d54fa02b9c6282144c3edf16a
ArvikVan <arvikvan@gmail.com>
2021-03-07 08:26:17 +0300

    5. Tracker - хранилище [#475881]коррективы

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -28,22 +28,13 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] itemsWithoutNull = new Item[items.length];
-        int count = 0;
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null) {
-                itemsWithoutNull[count] = items[i];
-                count++;
-            }
-        }
-        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
-        return itemsWithoutNull;
+     return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
         Item[] itemByName = new Item[items.length];
         int count = 0;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
                 itemByName[count] = items[i];
                 count++;
