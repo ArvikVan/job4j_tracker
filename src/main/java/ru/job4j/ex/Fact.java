1a85e96508fce82cbf7bd3906af5d4777512f980
ArvikVan <arvikvan@gmail.com>
2021-03-13 03:05:32 +0300

    0.3. Тестирование исключений с junit [#475822]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,10 +2,10 @@ package ru.job4j.ex;
 
 public class Fact {
     public static void main(String[] args) {
-
+        System.out.println("Factorial : " + calc(2));
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n must be greater then 0");
         }
